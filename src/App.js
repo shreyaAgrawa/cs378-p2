91ce0f2eca675fd821fcc0785cd98fe676df7be3
shreyaAgrawa <102440427+shreyaAgrawa@users.noreply.github.com>
2025-02-07 18:17:52 -0600

    code changed

--- src/App.js
+++ src/App.js
@@ -2,78 +2,86 @@ import './App.css';
 import MenuItem from './components/MenuItem';
 
 // import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
-
+import './App.css'
+import HeaderItem from './components/HeaderItem';
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
+const headerInfo = [
+  {
+    imageName: '/images/yoshi.png',
+    decorativeFont: 'Delicious Traditional Dishes to Feed the Soul',
+    secondHeader: 'The Best Japanese Restaurant in Austin!'
+  }
+]
 const menuItems = [
   {
     id: 1,
     title: 'Gyoza',
     description: 'Japanese dumplings',
-    imageName: 'gyoza.png',
+    imageName: '/images/gyoza.png',
     price: 5.99,
   },
   {
     id: 2,
     title: 'Sushi',
     description: 'Japanese rice rolls',
-    imageName: 'sushi.png',
+    imageName: '/images/sushi.png',
     price: 6.99,
   },
   {
     id: 3,
     title: 'Ramen',
-    description: 'Japanese noodle soup',
-    imageName: 'ramen.png',
+    description: 'Japanese noodle\n soup',
+    imageName: '/images/ramen.png',
     price: 7.99,
   },
   {
     id: 4,
     title: 'Matcha Cake',
-    description: 'Japanese green tea cake',
-    imageName: 'matcha-cake.png',
+    description: 'Japanese green\n tea cake',
+    imageName: '/images/matcha-cake.png',
     price: 4.99,
   },
   {
     id: 5,
     title: 'Mochi',
     description: 'Japanese rice cake',
-    imageName: 'mochi.png',
+    imageName: '/images/mochi.png',
     price: 3.99,
   },
   {
     id: 6,
     title: 'Yakitori',
-    description: 'Japanese skewered chicken',
-    imageName: 'yakitori.png',
+    description: 'Japanese skewered\n chicken',
+    imageName: '/images/yakitori.png',
     price: 2.99,
   },
   {
     id: 7,
     title: 'Takoyaki',
-    description: 'Japanese octopus balls',
-    imageName: 'takoyaki.png',
+    description: 'Japanese octopus\n balls',
+    imageName: '/images/takoyaki.png',
     price: 5.99,
   },
   {
     id: 8,
     title: 'Sashimi',
     description: 'Japanese raw fish',
-    imageName: 'sashimi.png',
+    imageName: '/images/sashimi.png',
     price: 8.99,
   },
   {
     id: 9,
     title: 'Okonomiyaki',
-    description: 'Japanese savory pancake',
-    imageName: 'okonomiyaki.png',
+    description: 'Japanese savory\n pancake',
+    imageName: '/images/okonomiyaki.png',
     price: 6.99,
   },
   {
     id: 10,
     title: 'Katsu Curry',
-    description: 'Japanese curry with fried pork',
-    imageName: 'katsu-curry.png',
+    description: 'Japanese curry\n with fried pork',
+    imageName: '/images/katsu-curry.png',
     price: 9.99,
   }
 ];
@@ -81,13 +89,29 @@ const menuItems = [
 
 function App() {
   return (
-    <div>
-      <h1>Menu</h1>
-      <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+    <>
+        <HeaderItem
+        name={"Yoshi"}
+        logo={headerInfo[0].imageName}
+        restaurant={headerInfo[0].decorativeFont}
+        description={headerInfo[0].secondHeader}
+        />
+
+      <div className="menu-center">
+        <div className="menu-items">
+          {menuItems.map((item) => (
+            <MenuItem 
+              key={item.id} 
+              title={item.title} 
+              description={item.description} 
+              price={item.price} 
+              imageName={item.imageName} 
+            />
+          ))}
+        </div>
       </div>
-    </div>
+
+      </>
   );
 }
 
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -5,11 +5,27 @@ import React from 'react';
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title }) => {
+const MenuItem = ({title, price, description, imageName}) => {
     return (
-        <div>
-            <h2>{title}</h2>
+        <>
+        <div className="menu-description">
+                <div>
+                    <img className="menu-photos" src={imageName} alt="image 1"/>
+                </div>
+                <div className="menu-description">
+                    <div className="menu-text">
+                        <b>{title}</b><br />
+                        <p style={{ whiteSpace: 'pre-line' }}>{description}<br /></p>
+                        <b>${price.toFixed(2)}</b>
+                    </div>
+                    <div>
+                        <button>
+                            Add
+                        </button>
+                    </div>
+                </div>
         </div>
+        </>
     );
 };
 
